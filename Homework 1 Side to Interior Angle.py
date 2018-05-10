881c012308425f6a39dc644494ab9ee38349a372
flash-drive <tenorio6@hawaii.edu>
2018-05-10 11:36:50 -1000

    Update Homework 1 Side to Interior Angle.py

--- Homework 1 Side to Interior Angle.py
+++ Homework 1 Side to Interior Angle.py
@@ -4,13 +4,17 @@
 # Write a program that calculates the number of sides given an interior angle.
 # Recall that the formula is thet = (180(n-2))/n
 
+# Function to calculate the number of sides given an interior angle
 def sides(angle):
     n = (-360)/(int(angle)-180)
     return n
 
+# Handle user input
 print('Please input an interior angle in degrees.')
 angle = input()
 
+# Call function
 side = sides(angle)
 
+# Print output
 print('A polygon with an interior angle of '+str(angle)+' degrees has '+str(side)+' sides.')
