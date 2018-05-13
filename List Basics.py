602b17203a32b914ae758c8e328fdc5559534afa
flash-drive <tenorio6@hawaii.edu>
2018-05-13 10:03:50 -1000

    Add files via upload

--- List Basics.py
+++ List Basics.py
@@ -1,4 +1,5 @@
 #### Lists Basics ####
+# Mark Tenorio
 
 
 #### Indexing ####
@@ -59,7 +60,18 @@
 # 'howdy' not in ['hello','hi','howdy','heyes']
 #>> False
 
-# For Loops with Lists, Multiple Assignments and Augmented Operators
+# name = 'Zophie'
+# 'Zo' in name
+# >>True
+# 'xxxx' in name
+# >>False
+
+# name = 'Zohpie'
+# for letter in name:
+# print(letter)
+# >>Z;o;p;h;i;e
+
+#### For Loops with Lists, Multiple Assignments and Augmented Operators ####
 
 #### For Loops #####
 
@@ -106,3 +118,61 @@
 
 # spam += 1
 # this adds 1 to spam. Can also do for multiplication, division, mod, subtraction
+
+#### List Methods ####
+
+# Works like a function but attached to a list.
+# Each data type has their own set of methods.
+# Each method can only be used within their respective data types     
+
+# spam = ['hello','hi','howdy','heyas']
+# spam.index('hello')
+# >>0
+# index('hello') will not work. Need to "attach it."
+# If the value is not within the index, it will return an exception.
+
+# spam = ['hello','hi','howdy','howdy']
+# spam.index('howdy')
+# >> 2 # will only return the first index it appears in.
+
+### Appending and Inserting Values in a List ###
+
+# spam = ['cat','dog','bat']
+# spam.append('moose')
+# >> ['cat','dog','bat','moose']
+
+# spam = ['cat','dog','bat']
+# spam.insert(1,'chicken')
+# >> ['cat','chickn','dog','bat']
+# Everything gets bumped up one.
+
+### Removing from a list ###
+
+# spam = ['cat','dog','bat','moose']
+# spam.remove('dog')
+# >>['cat','bat','moose']
+# This is differentf from the "del" operation because you
+# do not need to know the index.
+
+# spam = ['cat','bat','rat','hat','cat']
+# spam.remove('cat')
+# >>['bat','rat','hat','cat']
+# Will only remove the first instance of cat.
+
+#### Sorting ####
+
+# spam = [2,5,3.14,1,-7]
+# spam.sort()
+# >>[-7,1,2,3.4,5]
+# Works also for strings
+# Use spam.sort(reverse=True) to reverse sort
+# CANNOT sort a list with different data-types
+
+# spam = ['a','z','A','Z']
+# spam.sort(key=str.lower)
+# >>['A,'Z','a','z']
+# Sorting strings does NOT sort by alphabetical order
+# Instead it sorts by ASCII values, so uppercase will go first
+# spam.sort(key=str.lower)
+# >>['A','a','Z','z']
+# Will sort in "true" alphabetical order
