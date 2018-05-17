5a72df4e2e92e55568403d6f4066c71fb91ff6af
flash-drive <tenorio6@hawaii.edu>
2018-05-17 10:17:58 -1000

    Add files via upload

--- Homework 3 Weather Forecast String Replacement=INCOMPLETE.py
+++ Homework 3 Weather Forecast String Replacement=INCOMPLETE.py
@@ -6,7 +6,25 @@
 # The second input is a word that will be replaced in the list string.
 # The third input is the word that will replace the second input.
 
+# Function to replae the string
 def replacement(string,word1,word2):
-    string.find(word1)
-    string.find(word1,'')
+    index = string.find(word1)
+    lengther = index + len(word1)
+    output = string[:index] + word2 + string[lengther:]
+    return output
+
+# Handle user input
+print('Input string')
+string = input()
+print('input word to be changed in the string')
+word1 = input()
+print('Input word to replace the word to be replaced')
+word2 = input()
+
+# Call function
+result = replacement(string,word1,word2)
+
+# Print result
+print(result)
+    
     
