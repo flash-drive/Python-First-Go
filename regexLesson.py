bb23ed587d61ef60a5ecab5b69a8be0cf4c1db37
flash-drive <tenorio6@hawaii.edu>
2018-05-22 08:41:08 -1000

    Add files via upload

---
+++ regexLesson.py
@@ -0,0 +1,7 @@
+import re
+
+message = 'Call me 415-555-1011 tomorrow, or at 415-555-9999
+
+phoneNumRegex = re.compile(r'\d\d\d-\d\d\d-\d\d\d\d') #create regex object
+mo = phoneNumRegex.search(message) #create match object #use .findall to find all the phone number patterns
+print(mo.group()) #group gets matched string
