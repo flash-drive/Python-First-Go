7fbec32220ce501a477cd072721e7cef951c240d
Mark Tenorio <tenorio6@hawaii.edu>
2018-05-23 11:48:38 -1000

    Add files via upload

--- regexLesson2.py
+++ regexLesson2.py
@@ -1,7 +1,12 @@
 import re
 
-message = '45-55-11'
+message = '13-45-33-19-29-8'
 
-phoneNumRegex = re.compile(r'(\d\d)-\d\d-\d\d|/d/d/d') #create regex object #use pipe symbol as an "or." Basically a catch all.
-mo = phoneNumRegex.search(message) #create match object #use .findall to find all the phone number patterns
-print(mo.group()) #group gets matched string
+phoneNumRegex = re.compile(r'\d\d|\d-\d\d|\d-\d\d|\d-\d\d|\d-\d\d|\d-\d\d|\d') #create regex object #use pipe symbol as an "or." Basically a catch all.
+mo = phoneNumRegex.findall(message) #create match object #use .findall to find all the phone number patterns
+c = mo.group() #group gets matched string
+new = c.replace('-',',')
+print(type(new))
+listed = list(map(int,new.split(',')))
+print(listed)
+print(type(listed))
