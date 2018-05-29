71c12d5bbaa1a7706a2cd7ef72901cca2524c282
Mark Tenorio <tenorio6@hawaii.edu>
2018-05-28 15:03:04 -1000

    Add files via upload

---
+++ Data Structures and Arrays 1.py
@@ -0,0 +1,9 @@
+# Data Structures and Algorithims 1
+
+# Data Structures: Different ways of storing data on your computer.
+
+# Algorithims: Operations we can perform on different data structures and sets of instructions
+# for executing them.
+
+# The type of data structure you use can determine the type of algorithm you
+# come up with.
